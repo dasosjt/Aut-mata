0c896ebf4b50b60692c068e0ae9b781e625131de
Diego Sosa <diego.stt@live.com>
2016-08-08 13:11:18 -0600

    tree

--- exp-AFN/main.cpp
+++ exp-AFN/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
 
-int main(int argc, char** argv){
-  std::cout << "Have " << argc << " arguments:" << std::endl;
+int main(){
+  /*std::cout << "Have " << argc << " arguments:" << std::endl;
   for(int i = 0; i < argc; i++){
-    std::cout << argv[i] << std::endln;
-  }
+    std::cout << argv[i] << std::endl;
+  }*/
+  std::string exprsn;
+  std::cout << "Ingregsar expresion regular ";
+  getline (std::cin, exprsn);
+  std::cout << exprsn << "\n";
 }
