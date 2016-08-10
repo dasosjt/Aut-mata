cd993a612204ad0c2e40ded1e505ee3ebeb03ec9
Diego Sosa <diego.stt@live.com>
2016-08-10 17:33:58 -0600

    Boost Tree

--- exp-AFN/main.cpp
+++ exp-AFN/main.cpp
@@ -10,6 +10,16 @@ int main(){
   boost::property_tree::ptree tree;
   tree.put("test_name", "test_value");
 
+  boost::property_tree::ptree child;
+
+  for(int i = 0; i < 32; ++i){
+    std::stringstream ss;
+    ss << "value" << i;
+    child.put(ss.str(), i);
+  }
+
+  tree.add_child("some values", child);
+
   boost::property_tree::write_json("./output.json", tree);
 
   return 0;
