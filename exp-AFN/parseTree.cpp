a49745b5571261f578d9062198799211ef8bc3c7
Diego Sosa <diego.stt@live.com>
2016-08-11 13:30:42 -0600

    find_last_of

--- exp-AFN/parseTree.cpp
+++ exp-AFN/parseTree.cpp
@@ -1,6 +1,7 @@
 //g++ parseTree.cpp -o testBoost -Lc:\Boostbuild\lib -Ic:\Boostbuild\include\boost-1_6
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -8,37 +9,33 @@
 #include <boost/property_tree/json_parser.hpp>
 
 //Funcion recursiva para armar el arbol
-boost::property_tree::ptree parseToTree(std::vector<char> tokens){
+boost::property_tree::ptree parseToTree(std::vector<char> tokens, std::string c){
   //Un nuevo child de un arbol
   boost::property_tree::ptree child;
-  //Si el vector es mayor a 1, entonces separamos el arbol.
-  if(tokens.size() > 1){
+
+  /*if(tokens[0] == '('){
     std::vector<char> subtokens(tokens.begin()+1, tokens.end());
-    child.put("0", tokens[0]);
-    child.add_child("1", parseToTree(subtokens));
-  } else {
-    child.put("1", tokens[0]);
-  }
+    child.add_child("()", parseToTree(subtokens, "2"));
+  }*/
+
   return child;
 }
 
 int main(){
   //Creamos la raiz principal de arbol
   boost::property_tree::ptree tree;
-  tree.put("test_name", "test_value");
 
-  std::string str("Spjdst");
+  std::string str("(a)");
   std::vector<char> tokens(str.begin(), str.end());
 
-  for(int i = 0; i < tokens.size(); ++i){
-    std::cout << tokens[i];
-  }
-
   //invertimos el vector
-  std::reverse(tokens.begin(),tokens.end());
+  /*std::reverse(tokens.begin(),tokens.end());*/
+  /*it = std::find_end (tokens.begin(), tokens.end());
+  std::cout << it << std::endl;*/
 
-  tree.add_child("some values", parseToTree(tokens));
-  boost::property_tree::write_json("./output.json", tree);
-
+  /*tree.add_child("parseToTree", parseToTree(tokens, "1"));
+  boost::property_tree::write_json("./output.json", tree);*/
+  std::string::size_type last = str.find_last_of(")");
+  std::cout << last;
   return 0;
 }
